bc334b89e6db54c3f73fe157f8c713eaef1f9423
Marcin <mardab1994@gmail.com>
2019-09-24 22:31:53 +0200

    Dodanie modulu z filtrami.
    Dodanie progowania.

---
+++ src/filters/filters.c
@@ -0,0 +1,33 @@
+/*
+ * filters.c
+ *
+ *  Created on: 24 wrz 2019
+ *      Author: marcinsith
+ */
+
+#include "filters.h"
+#include "../errors.h"
+
+int
+tresholding(image_t *image, int treshold)
+{
+	if (image == NULL) {
+		return NO_IMAGE_PTR;
+	}
+	treshold = (treshold * image->shades_of_grey) / 100;
+
+    for (int y = 0; y < image->y_dim; y++) {
+		for (int x = 0; x < image->x_dim; x++) {
+			if (image->matrix[y * image->x_dim + x] <= treshold) {
+				image->matrix[y * image->x_dim + x]  = 0;
+			} else {
+				image->matrix[y * image->x_dim + x] = image->shades_of_grey;
+			}
+		}
+	}
+
+    return 0;
+}
+
+
+
---
+++ src/filters/filters.h
@@ -0,0 +1,16 @@
+/*
+ * filters.h
+ *
+ *  Created on: 24 wrz 2019
+ *      Author: marcinsith
+ */
+
+#ifndef SRC_FILTERS_FILTERS_H_
+#define SRC_FILTERS_FILTERS_H_
+
+#include "../config.h"
+#include "../image/image.h"
+
+int tresholding(image_t *image, int treshold);
+
+#endif /* SRC_FILTERS_FILTERS_H_ */
