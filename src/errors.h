9d8c8e57f3dd85e522f19846d3a3feaf61da43c7
Marcin <mardab1994@gmail.com>
2019-09-24 22:30:34 +0200

    Dodanie podstawowych bledow.

---
+++ src/errors.h
@@ -0,0 +1,17 @@
+/*
+ * errors.h
+ *
+ *  Created on: 24 wrz 2019
+ *      Author: marcinsith
+ */
+
+#ifndef SRC_ERRORS_H_
+#define SRC_ERRORS_H_
+
+typedef enum {
+	INPUT_FILE = 1,
+	NO_IMAGE_PTR,
+	LAST_ERROR
+} errors_t;
+
+#endif /* SRC_ERRORS_H_ */
