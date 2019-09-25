7b7e996849a762be503c83576ab14fea4d664bd1
Marcin <mardab1994@gmail.com>
2019-09-25 21:00:18 +0200

    Dodanie nowych filtrów.
    +półprogowanie bieli/czerni
    +korekta gamma
    +zmiana poziomów

--- src/filters/filters.c
+++ src/filters/filters.c
@@ -8,6 +8,8 @@
 #include "filters.h"
 #include "../errors.h"
 
+#include <math.h>
+
 int
 tresholding(image_t *image, int treshold)
 {
@@ -29,6 +31,77 @@ tresholding(image_t *image, int treshold)
 
     return 0;
 }
+
+int
+half_tresholding(image_t *image, int treshold, color_tresholding_t color_tresholding)
+{
+	if (image == NULL) {
+		return NO_IMAGE_PTR;
+	}
+
+	treshold = (treshold * image->shades_of_grey) / 100;
+
+	for (int y = 0; y < image->y_dim; y++) {
+		for (int x = 0; x < image->x_dim; x++) {
+			if (color_tresholding == BLACK) {
+				if (image->matrix[y * image->x_dim + x] <= treshold) {
+					image->matrix[y * image->x_dim + x]  = 0;
+				}
+			} else if (color_tresholding == WHITE) {
+				if (image->matrix[y * image->x_dim + x] > treshold) {
+					image->matrix[y * image->x_dim + x]  = image->shades_of_grey;
+				}
+			}
+		}
+	}
+
+	return 0;
+}
+
+int
+gamma_correction(image_t *image, double gamma_level)
+{
+	if (image == NULL) {
+		return NO_IMAGE_PTR;
+	}
+
+	for (int y = 0; y < image->y_dim; y++) {
+		for (int x = 0; x < image->x_dim; x++) {
+			image->matrix[y * image->x_dim + x]  = image->shades_of_grey *
+					pow(((image->matrix[y * image->x_dim + x])/image->shades_of_grey), (1 / gamma_level));
+		}
+	}
+
+	return 0;
+}
+
+int
+change_levels(image_t *image, int black, int white)
+{
+	if (image == NULL) {
+		return NO_IMAGE_PTR;
+	}
+
+	if (black > 255 || white > 255) {
+		return COLOR_LEVEL_OVERFLOW;
+	}
+
+	for (int y = 0; y < image->y_dim; y++) {
+		for (int x = 0; x < image->x_dim; x++) {
+			if (image->matrix[y * image->x_dim + x] <= black) {
+				image->matrix[y * image->x_dim + x] = 0;
+			} else if (image->matrix[y * image->x_dim + x] >= white) {
+				image->matrix[y * image->x_dim + x] = image->shades_of_grey;
+			} else {
+				image->matrix[y * image->x_dim + x] = (image->matrix[y * image->x_dim + x] - black) *
+						(image->shades_of_grey / (white - black));
+			}
+		}
+	}
+
+	return 0;
+}
+
 
 
 
--- src/filters/filters.h
+++ src/filters/filters.h
@@ -11,10 +11,20 @@
 #include "../config.h"
 #include "../image/image.h"
 
+#include <stdbool.h>
+
 typedef enum {
 	WHITE = 0,
 	BLACK
 } color_tresholding_t;
+
+
+
 int tresholding(image_t *image, int treshold);
+int half_tresholding(image_t *image, int treshold, color_tresholding_t color_tresholding);
+int gamma_correction(image_t *image, double gamma_level);
+int change_levels(image_t *image, int black, int white);
+
+
 
 #endif /* SRC_FILTERS_FILTERS_H_ */
