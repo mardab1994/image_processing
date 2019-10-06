7ca633d6ad9ad0881e8e289b9e8b22fb2a02018f
Marcin <mardab1994@gmail.com>
2019-10-06 17:49:21 +0200

    Uproszczenie maina - korzystanie modulu menu

--- src/image_processing.c
+++ src/image_processing.c
@@ -14,53 +14,16 @@
 #include "image/image.h"
 #include "filters/filters.h"
 
+#include "menu/menu.h"
+
 int main (int argc, char **argv) {
 	if (argc != 2) {
 		printf("Bledna liczba argumentow\n");
 		return 1;
 	}
 
-	printf("%s\n", argv[1]);
-
 	read_image(argv);
-
-	printf_image_info();
-
-#if (DEBUG == 1)
-    print_image_matrix();
-#endif
-
-    if (tresholding(image_get(), 50) != 0) {
-    	printf("ERROR\n");
-    }
-
-    if (half_tresholding(image_get(),55 , BLACK) != 0) {
-    	printf("ERROR\n");
-    }
-
-    if (gamma_correction(image_get(), 25) != 0) {
-    	printf("ERROR\n");
-    }
-
-    if (change_levels(image_get(), 20, 30) != 0) {
-    	printf("ERROR\n");
-    }
-
-    if (contouring(image_get()) != 0) {
-    	printf("ERROR\n");
-    }
-
-    if (blurr(image_get(), BLURR_ORBITAL, 1) != 0) {
-    	printf("ERROR\n");
-    }
-
-    if (hstogram_stretching(image_get()) != 0) {
-    	printf("ERROR\n");
-    }
-
-    if (histgram_alignment(image_get()) != 0) {
-    	printf("ERROR\n");
-    }
+	main_menu();
 
 	return 0;
 }
